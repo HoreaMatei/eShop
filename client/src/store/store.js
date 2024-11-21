1d417f986943d39d74043223ca214e227c3ad06e
HoreaMatei <mateihorea17@yahoo.com>
2024-11-21 16:46:04 +0200

    address

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,13 +3,14 @@ import authReducer from "./auth-slice";
 import AdminProductsSlice from "./admin/products-slice";
 import shoppingProductSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
-
+import shopAddresstSlice from "./shop/address-slice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     adminProducts: AdminProductsSlice,
     shopProducts: shoppingProductSlice,
     shopCart: shopCartSlice,
+    shopAddress: shopAddresstSlice,
   },
 });
 
