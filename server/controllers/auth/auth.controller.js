630b486bb78f148740a5823ca3b9b17894d98b2f
HoreaMatei <mateihorea17@yahoo.com>
2025-01-12 13:55:28 +0200

    cookies change

--- server/controllers/auth/auth.controller.js
+++ server/controllers/auth/auth.controller.js
@@ -63,7 +63,7 @@ const loginUser = async (req, res) => {
       { expiresIn: "60m" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    res.cookie("token", token, { httpOnly: true, secure: true }).json({
       succes: true,
       message: "Logged in succesfully",
       user: {
