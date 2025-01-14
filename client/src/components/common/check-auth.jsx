a10b5230166c71c9324d3384b135cd9fd61a7f0b
HoreaMatei <mateihorea17@yahoo.com>
2025-01-14 16:19:36 +0200

    auth changes

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -5,55 +5,49 @@ import { Navigate } from "react-router-dom";
 const CheckAuth = ({ isAuthenticated, user, children }) => {
   const location = useLocation();
 
-  if (location.pathname === "/") {
-    if (!isAuthenticated) {
-      return <Navigate to="/auth/login" />;
-    } else {
-      if (user?.role === "admin") {
-        return <Navigate to="/admin/dashboard" />;
-      } else {
-        return <Navigate to="/shop/home" />;
-      }
-    }
-  }
-
-  if (
-    !isAuthenticated &&
-    !(
+  // Redirect to login if not authenticated
+  if (!isAuthenticated) {
+    // Allow access to login and register routes
+    if (
       location.pathname.includes("/login") ||
       location.pathname.includes("/register")
-    )
-  ) {
-    return <Navigate to="/auth/login" />;
+    ) {
+      return <>{children}</>;
+    }
+    // Redirect all other unauthenticated users to login
+    return <Navigate to="/auth/login" replace />;
   }
+
+  // Handle redirects for authenticated users trying to access login/register
   if (
     isAuthenticated &&
     (location.pathname.includes("/login") ||
       location.pathname.includes("/register"))
   ) {
     if (user?.role === "admin") {
-      return <Navigate to="/admin/dashboard" />;
-    } else {
-      return <Navigate to="/shop/home" />;
+      return <Navigate to="/admin/dashboard" replace />;
     }
+    return <Navigate to="/shop/home" replace />;
   }
 
+  // Handle role-based access
   if (
     isAuthenticated &&
     user?.role !== "admin" &&
-    location.pathname.includes("admin")
+    location.pathname.includes("/admin")
   ) {
-    return <Navigate to="/unauth-page" />;
+    return <Navigate to="/unauth-page" replace />;
   }
 
   if (
     isAuthenticated &&
     user?.role === "admin" &&
-    location.pathname.includes("shop")
+    location.pathname.includes("/shop")
   ) {
-    return <Navigate to="/admin/dashboard" />;
+    return <Navigate to="/admin/dashboard" replace />;
   }
 
+  // Default behavior: Render children if no redirection is required
   return <>{children}</>;
 };
 
