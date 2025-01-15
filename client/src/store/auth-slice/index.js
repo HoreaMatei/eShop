82f598df5de740a1e09924bf0ccc9dee6c5a3d9b
HoreaMatei <mateihorea17@yahoo.com>
2025-01-15 17:27:27 +0200

    auth slice change2

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -105,7 +105,7 @@ const authSlice = createSlice({
       .addCase(loginUser.fulfilled, (state, action) => {
         state.isLoading = false;
         state.user = action.payload.succes ? action.payload.user : null;
-        state.isAuthenticated = action.payload.succes ? true : false;
+        state.isAuthenticated = action.payload.succes;
         state.token = action.payload.token;
         sessionStorage.setItem("token", JSON.stringify(action.payload.token));
       })
