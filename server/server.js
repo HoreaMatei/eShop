61f80ba0e2c2bc9f99b23af7eb1efaf8b7d23a4f
HoreaMatei <mateihorea17@yahoo.com>
2025-01-15 16:22:24 +0200

    last commit

--- server/server.js
+++ server/server.js
@@ -4,18 +4,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const authRouter = require("./routes/auth/auth-routes");
-const adminProductsRouter = require("./routes/admin/products-routes");
-const adminOrderRouter = require("./routes/admin/order-routes");
+const authRouter = require("../routes/auth/auth-routes");
+const adminProductsRouter = require("../routes/admin/products-routes");
+const adminOrderRouter = require("../routes/admin/order-routes");
 
-const shopProductsRouter = require("./routes/shop/products-routes");
-const shopCartRouter = require("./routes/shop/cart-routes");
-const shopAddressRouter = require("./routes/shop/address-routes");
-const shopOrderRouter = require("./routes/shop/order-routes");
-const shopSearchRouter = require("./routes/shop/search-routes");
-const shopReviewRouter = require("./routes/shop/review-routes");
+const shopProductsRouter = require("../routes/shop/products-routes");
+const shopCartRouter = require("../routes/shop/cart-routes");
+const shopAddressRouter = require("../routes/shop/address-routes");
+const shopOrderRouter = require("../routes/shop/order-routes");
+const shopSearchRouter = require("../routes/shop/search-routes");
+const shopReviewRouter = require("../routes/shop/review-routes");
 
-const commonFeatureRouter = require("./routes/common/feature-routes");
+const commonFeatureRouter = require("../routes/common/feature-routes");
 
 //create a database connection -> u can also
 //create a separate file for this and then import/use that file here
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: process.env.CLIENT_BASE_URL,
-    methods: ["GET", "POST", "DELETE", "PUT"],
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
     allowedHeaders: [
       "Content-Type",
       "Authorization",
