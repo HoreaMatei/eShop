eb6721c3b24d45a068d8e51f519380f39c2f558d
HoreaMatei <mateihorea17@yahoo.com>
2025-01-17 15:26:35 +0200

    222222

--- server/controllers/auth/auth.controller.js
+++ server/controllers/auth/auth.controller.js
@@ -111,7 +111,7 @@ const logoutUser = (req, res) => {
 //auth middleware
 const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = authHeader && authHeader.split(" ")[0];
   if (!token)
     return res.status(401).json({
       success: false,
