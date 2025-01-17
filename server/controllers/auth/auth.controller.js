23a1bfea0f3889a4c8c1907d106ac8de941fead8
HoreaMatei <mateihorea17@yahoo.com>
2025-01-17 18:25:33 +0200

    error

--- server/controllers/auth/auth.controller.js
+++ server/controllers/auth/auth.controller.js
@@ -110,8 +110,7 @@ const logoutUser = (req, res) => {
 
 //auth middleware
 const authMiddleware = async (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-
+  const authHeader = req.headers["Authorization"];
   const token = authHeader && authHeader.split(" ")[1];
   if (!token)
     return res.status(401).json({
