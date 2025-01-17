67eba4961e47e22afc95a9996012b3a5efda1931
HoreaMatei <mateihorea17@yahoo.com>
2025-01-17 15:14:07 +0200

    222

--- server/controllers/auth/auth.controller.js
+++ server/controllers/auth/auth.controller.js
@@ -68,9 +68,21 @@ const loginUser = async (req, res) => {
       { expiresIn: "60m" }
     );
 
-    res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    // res.cookie("token", token, { httpOnly: true, secure: false }).json({
+    //   success: true,
+    //   message: "Logged in successfully",
+    //   user: {
+    //     email: checkUser.email,
+    //     role: checkUser.role,
+    //     id: checkUser._id,
+    //     userName: checkUser.userName,
+    //   },
+    // });
+
+    res.status(200).json({
       success: true,
-      message: "Logged in successfully",
+      message: "Logged in",
+      token,
       user: {
         email: checkUser.email,
         role: checkUser.role,
@@ -78,18 +90,6 @@ const loginUser = async (req, res) => {
         userName: checkUser.userName,
       },
     });
-
-    // res.status(200).json({
-    //    success: true,
-    //   message: "Logged in successfuly",
-    //  token,
-    //   user: {
-    //   email: checkUser.email,
-    //   role: checkUser.role,
-    //     id: checkUser._id,
-    //     userName: checkUser.userName,
-    //    },
-    //  });
   } catch (e) {
     console.log(e);
     res.status(500).json({
